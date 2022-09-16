e6798b1e5889afbcb3b43850358f0ccd809195f4
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2022-09-15 19:28:28 -0700

    Update run_all_notebooks.py

--- run_all_notebooks.py
+++ run_all_notebooks.py
@@ -28,7 +28,7 @@ def main(root_dir):
         "fasttext_amazon_reviews",  # takes very long to run
         "multiannotator_cifar10",  # requires GPU
         "outlier_detection_cifar10",  # requires GPU
-        "entity_recognition",  # requires GPU
+        "entity_recognition",  # requires GPU, does not use cleanlab
         "cnn_coteaching_cifar10",  # no ipynb, only a py file
     ]
 
